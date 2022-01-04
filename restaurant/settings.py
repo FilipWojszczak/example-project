bed50b65a1258ba6b283805c6c5541a040beff9f
FilipWojszczak <filip.wojszczak@gmail.com>
2022-01-04 21:19:14 +0100

    Rename project

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'nandiasgarden.urls'
+ROOT_URLCONF = 'restaurant.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'nandiasgarden.wsgi.application'
+WSGI_APPLICATION = 'restaurant.wsgi.application'
 
 
 # Database
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,4 +1,4 @@
-"""nandiasgarden URL Configuration
+"""restaurant URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
